1a14eda8109e929e980bfee585de6d4247786d06
Hung Vu <vudinhhung942k@gmail.com>
2025-12-01 18:07:54 -0500

    Add zsh configs

---
+++ full.zsh
@@ -0,0 +1,12 @@
+source ~/dotfiles/minimal.sh
+
+# | yazi |------------------------------------------------------------------------------
+if command -v yazi >/dev/null 2>&1; then
+  function y() {
+    local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+    yazi "$@" --cwd-file="$tmp"
+    IFS= read -r -d '' cwd < "$tmp"
+    [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+    rm -f -- "$tmp"
+  }
+fi
